50d8247c9fb383352e145c99814bf758cac2c7b2
Game Shared <GameDesignShared@mac-lab-d25tq030gg7d-9196.local>
2019-01-15 13:56:48 -0600

    rr

--- AITestBed/SpringbokClass.swift
+++ AITestBed/SpringbokClass.swift
@@ -62,4 +62,47 @@ class SpringbokClass:EntityClass
         age=random(min: 1.0, max: MAXAGE*0.7)
         
     } // full init()
+    
+    init(theScene:SKScene, theMap: MapClass, pos: CGPoint, number: Int, leader: EntityClass?)
+    {
+        super.init()
+        
+        if leader==nil
+        {
+            isHerdLeader = true
+        }
+        
+        else
+        {
+            herdLeader=leader
+        }
+        
+        // set the passed references
+        map=theMap
+        scene=theScene
+        
+        
+        // sprite update
+        sprite=SKSpriteNode(imageNamed: "Springbok")
+        sprite.position=pos
+        sprite.name="Springbok"
+        sprite.name=String(format:"entSpringbok%04d", number)
+        name=String(format:"entSpringbok%04d", number)
+        sprite.zPosition=140
+        
+        scene!.addChild(sprite)
+        
+        // Variable updates
+        MAXSPEED=2.2
+        TURNRATE=0.2
+        TURNFREQ=0.06
+        AICycle=0
+        MAXAGE=7*8640
+        MAXAGE=random(min: MAXAGE*0.8, max: MAXAGE*1.4) // adjust max age to the individual
+        age=random(min: 1.0, max: MAXAGE*0.7)
+        
+    } // full init()
 } // class SpringbokClass
+
+
+
